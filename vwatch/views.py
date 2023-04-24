5a2c8e5ee92c040cc40a3ee1ce232523b9f75c17
goisbrunoaugusto <brunogois902@gmail.com>
2023-04-24 11:55:18 -0300

    injecting data

--- vwatch/views.py
+++ vwatch/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from utils.vwatch.factory import make_vwatch
 
 
 def home(request):
-    return render(request, 'vwatch/pages/home.html')
+    return render(request, 'vwatch/pages/home.html', context={
+        'vwatch': [make_vwatch() for _ in range(10)],
+    })
 
 
 def evento(request, id):
-    return render(request, 'vwatch/pages/evento.html')
+    return render(request, 'vwatch/pages/evento.html', context={
+        'vwatch': [make_vwatch()],
+    })
